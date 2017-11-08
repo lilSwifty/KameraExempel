bff107cdb817d1d054fba2ac39952a3291e67f3e
Manolo <manisedighi@icloud.com>
2017-11-08 10:18:33 +0100

    Initial commit

---
+++ app/src/main/java/com/iths/manisedighi/kameraexempel/SaveImagesSQL/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.iths.manisedighi.kameraexempel.SaveImagesSQL;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.iths.manisedighi.kameraexempel.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
